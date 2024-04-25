b9fe9969b8c272bd51dd6ffc1a206a397a30c861
Dmytro Katashev <katashov.dima@gmail.com>
2024-04-25 14:28:38 +0300

    feat: Improved SFTPService

--- cartridges/lib_webservice/cartridge/scripts/webservice/SFTPService.js
+++ cartridges/lib_webservice/cartridge/scripts/webservice/SFTPService.js
@@ -8,22 +8,37 @@ var BaseService = require('*/cartridge/scripts/webservice/BaseService');
  */
 var SFTPService = BaseService.extend({
   /**
-   * Creates a request for the SFTP service.
+   * Creates a protocol-specific client object.
    *
-   * @param {dw.svc.FTPService} svc - The FTP service instance.
-   * @param {SFTPParams} params - The parameters for creating the request.
+   * @param {dw.svc.FTPService} svc - The (S)FTP service instance.
    * @returns {SFTPParams} The input parameters.
    */
-  createRequest: function (svc, params) {
-    this._getServiceCredential(svc, params);
+  initServiceClient: function (svc) {
+    var profile = svc.configuration.profile;
+    var credential = this._getServiceCredential(svc);
+    var match = credential.URL.match(/^(s?ftp):\/\/([^/:]+)/);
 
-    return params;
+    if (!match) {
+      throw new Error('(S)FTP URL is invalid!');
+    }
+
+    var protocolType = match[1];
+    var host = match[2];
+    var client = this._getClient(protocolType);
+
+    client.connect(host, credential.user, credential.password);
+
+    if (profile.timeoutMillis) {
+      client.setTimeout(profile.timeoutMillis);
+    }
+
+    return client;
   },
 
   /**
-   * Executes the SFTP service operations.
+   * Executes the (S)FTP service operations.
    *
-   * @param {dw.svc.FTPService} svc - The FTP service instance.
+   * @param {dw.svc.FTPService} svc - The (S)FTP service instance.
    * @param {SFTPParams} params - The parameters for executing the operations.
    * @returns {*} The result of the executed operation.
    * @throws {Error} If no valid operation with arguments or execute callback is provided.
@@ -47,16 +62,36 @@ var SFTPService = BaseService.extend({
   },
 
   /**
-   * Retrieves the service credential for the SFTP service.
+   * Retrieves the service credential for the (S)FTP service.
    *
    * @protected
-   * @param {dw.svc.FTPService} svc - The FTP service instance.
-   * @param {SFTPParams} params - The SFTP parameters.
+   * @param {dw.svc.FTPService} svc - The (S)FTP service instance.
    * @returns {dw.svc.ServiceCredential} The service credential.
    */
   // eslint-disable-next-line no-unused-vars
-  _getServiceCredential: function (svc, params) {
+  _getServiceCredential: function (svc) {
     return svc.configuration.credential;
+  },
+
+  /**
+   * Return an FTP or SFTP client
+   *
+   * @protected
+   * @param {string} protocolType
+   * @returns {dw.svc.FTPClient|dw.svc.SFTPClient}
+   */
+  _getClient: function (protocolType) {
+    var FTPClient = require('dw/net/FTPClient');
+    var SFTPClient = require('dw/net/SFTPClient');
+
+    switch (protocolType) {
+      case 'ftp':
+        return new FTPClient();
+      case 'sftp':
+        return new SFTPClient();
+      default:
+        throw new Error('(S)FTP URL is invalid!');
+    }
   }
 });
 
